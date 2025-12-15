6b5c0fad9b26778ae29d2e9e9ab80fc897e95e14
Jarred Barber <jpb5082@gmail.com>
2025-12-15 11:53:29 -0500

    stuff

--- site_config.py
+++ site_config.py
@@ -46,10 +46,6 @@ PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 INDEX_SAVE_AS = 'index.html'
 
-# Direct templates
-DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'newsletter']
-NEWSLETTER_SAVE_AS = 'newsletter.html'
-
 
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['speaker_highlight', 'show_segments']
